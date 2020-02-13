9b1869c2f8bbf4e074e9e99279171bc0003993d6
Leandro <gringoperalta@gmail.com>
2020-02-14 10:54:58 +1300

    Simplify example.

--- example/src/main/java/nz/co/vilemob/daggerviewmodel/example/main/MainModule.kt
+++ example/src/main/java/nz/co/vilemob/daggerviewmodel/example/main/MainModule.kt
@@ -1,18 +1,12 @@
 package nz.co.vilemob.daggerviewmodel.example.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.android.ContributesAndroidInjector
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import nz.co.vilemob.daggerviewmodel.example.ActivityScope
 import nz.co.vilemob.daggerviewmodel.example.pop.PopFragment
 import nz.co.vilemob.daggerviewmodel.example.push.PushFragment
-import java.util.concurrent.TimeUnit
 import javax.inject.Named
 
 @Module
@@ -26,31 +20,18 @@ abstract class MainModule {
 
     @Binds
     @ActivityScope
-    @Named("scoped_live_data")
-    abstract fun scopedLiveData(intLiveData: LiveData<Int>): LiveData<Int>
+    @Named("scoped_int")
+    abstract fun scopedInt(number: Int): Int
 
     @Binds
-    @Named("unscoped_live_data")
-    abstract fun unscopedLiveData(intLiveData: LiveData<Int>): LiveData<Int>
+    @Named("unscoped_int")
+    abstract fun unscopedInt(number: Int): Int
 
     @Module
     companion object {
 
         @JvmStatic
         @Provides
-        fun intervalObservable(): Observable<Int> = Observable
-                .interval(1, TimeUnit.SECONDS)
-                .scan(0, { previous, _ -> previous + 1 })
-
-        @JvmStatic
-        @Provides
-        fun intervalLiveData(observable: Observable<Int>): LiveData<Int> {
-            val timerMutableLiveData = MutableLiveData<Int>()
-            observable
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { timerMutableLiveData.value = it }
-            return timerMutableLiveData
-        }
+        fun number(): Int = (1..100).random()
     }
 }
--- example/src/main/java/nz/co/vilemob/daggerviewmodel/example/pop/PopFragment.kt
+++ example/src/main/java/nz/co/vilemob/daggerviewmodel/example/pop/PopFragment.kt
@@ -1,11 +1,10 @@
 package nz.co.vilemob.daggerviewmodel.example.pop
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_pop.*
 import nz.co.vilemob.daggerviewmodel.ViewModelFragment
 import nz.co.vilemob.daggerviewmodel.example.R
@@ -22,12 +21,7 @@ class PopFragment : ViewModelFragment<PopViewModel>() {
             viewModelProvider.get(PopViewModel::class.java)
 
     override fun onViewModelCreated(viewModel: PopViewModel) {
-        viewModel.scopedLiveData.observe(this, Observer {
-            scopedTimerTextView.text = getString(R.string.scoped_timer, it)
-        })
-
-        viewModel.unscopedLiveData.observe(this, Observer {
-            unscopedTimerTextView.text = getString(R.string.unscoped_timer, it)
-        })
+        scopedNumberTextView.text = getString(R.string.scoped_number, viewModel.scopedInt)
+        unscopedNumberTextView.text = getString(R.string.unscoped_number, viewModel.unscopedInt)
     }
 }
